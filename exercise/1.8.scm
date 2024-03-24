800e131816598e8fb00f85ee92c4ccfc40cc0d35
bybogdan <bgdnvarlamov@gmail.com>
2024-03-24 20:30:53 +0400

    Add notes

---
+++ exercise/1.8.scm
@@ -0,0 +1,10 @@
+; Exercise 1.8:
+; Newton’s method for cube roots is based on the fact that if y
+; is an approximation to the cube root of x
+;, then a better approximation is given by the value
+;x/y2+2y3.
+;Use this formula to implement a cube-root procedure analogous to the square-root procedure. (In 1.3.4 we will see how to implement Newton’s method in general as an abstraction of these square-root and cube-root procedures.)
+; https://sarabander.github.io/sicp/html/1_002e1.xhtml#Exercise-1_002e8
+
+; Answer:
+-
